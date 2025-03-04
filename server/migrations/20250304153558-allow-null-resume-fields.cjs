ea90d0831309b9982607b5c5450907a1fb97080e
Nathan Green <ngreen1632@gmail.com>
2025-03-04 10:27:33 -0600

    Allow null values for 'title' field in Resumes table
    This migration updates the 'title' field in the 'Resumes' table to allow null values. The change ensures greater flexibility for cases where a title is not provided. A down method is included to restore the previous constraint if needed.

---
+++ server/migrations/20250304153558-allow-null-resume-fields.cjs
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('Resumes', 'title', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('Resumes', 'title', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+  },
+};
