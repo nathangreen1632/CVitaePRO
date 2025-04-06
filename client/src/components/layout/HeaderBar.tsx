2d2534a70c17a864f37b39b6bd872c80a6e2c981
Nathan Green <ngreen1632@gmail.com>
2025-04-06 00:22:33 -0500

    Relocate HeaderBar component and update AuthContext import
    Moved the HeaderBar component to a new "layout" directory for better organization. Adjusted the AuthContext import path to reflect the updated file structure.

--- client/src/components/layout/HeaderBar.tsx
+++ client/src/components/layout/HeaderBar.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../../context/AuthContext.tsx";
 import { useNavigate } from "react-router-dom";
 
 interface HeaderBarProps {
