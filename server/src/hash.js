37bed78018f60e7b2e32b56da2bcb33692615dc1
Nathan Green <ngreen1632@gmail.com>
2025-04-06 00:22:25 -0500

    Remove unused password hashing utility
    Deleted a test utility for password hashing that was no longer needed. This cleanup helps maintain code simplicity and avoid potential confusion.

--- server/src/hash.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const hashPassword = async () => {
-    const hashedPassword = await bcrypt.hash('NewSecurePass456', 12);
-    console.log('Hashed Password:', hashedPassword);
-};
-
-hashPassword();
